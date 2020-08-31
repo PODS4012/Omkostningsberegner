770a230e2bc21a1d87eef9b75e81202536272a27
D- CODE <darek_sokol@hotmail.com>
2020-08-31 22:26:05 +0200

    Add project files.

---
+++ Beregner/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beregner
+{
+    public class Client
+    {
+        public string Name { get; set; } = "Birges";
+        public double Consumption { get; set; } = 100704.1;
+        public double Income { get; set; } = 246585;
+
+        public Client(){}
+        public Client(string Name, double Consumption, double Income)
+        {
+            this.Name = Name;
+            this.Consumption = Consumption;
+            this.Income = Income;
+        }
+    }
+}
---
+++ Beregner/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Beregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool askUser = AskUser();
+
+            var client = CreateClient(askUser);
+
+            Console.WriteLine($"Name: {client.Name}, Income: {client.Income}, Consumption: {client.Consumption}");
+        }
+
+        public static Client CreateClient(bool type)
+        {
+            if (!type)
+            {
+                Client client = new Client();
+                return client;  
+            }
+            else
+            {
+                Client client = new Client();
+                return client;
+            }
+        }
+
+        public static bool AskUser()
+        {
+            Console.WriteLine("Create default client 0 or 1 for userdefined");
+             bool selection = bool.Parse(Console.ReadLine());
+            return selection;
+        }
+    }
+}
